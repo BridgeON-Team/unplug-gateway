8edd4fb822dcd6b833ebc92e2e67600e9b9a644e
minjung <alswjd0240@naver.com>
2025-09-18 19:31:03 +0900

    initial commit

---
+++ src/main/java/org/example/unpluggatewayservice/UnplugGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.unpluggatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnplugGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnplugGatewayServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/unpluggatewayservice/UnplugGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.unpluggatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnplugGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
